40ce2d5da2bc6b9debfc8281d6c2e0648d1913de
mustfakara <mustikara7434@gmail.com>
2023-11-12 22:05:29 +0300

    TASK 7 : Added Login Method
    - Added POST Method to authorize the login session. If exist,
    the user can log in.
    
    - Added the response screenshot on PostMan.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,48 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+  {"username": "admin", "password": "admin"},
+];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+
+// This function returns true or false in case of registering the user or in login function
+
+// This is on Task 6 and 7 *****
+
+const isValid = (username)=>{ 
+  let existedUser = users.filter((user) => {
+    return user.username === username;
+  });
+  return existedUser.length > 0;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+  let validUsers = users.filter((user) => user.username === username && user.password === password);
+  return validUsers.length > 0;
 }
 
-//only registered users can login
+//Here, the login method checks the authentication of the body requems. If they are valid,
+//you are in.
+
+// This is in Task 7 *****
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Error logging in. Please check username and password!"})
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', {expiresIn: 60 * 60});
+
+    req.session.authorization = { accessToken, username }
+    return res.json({message: "User successfully logged in!"})
+  }
+  return res.status(208).json({message: "Invalid Login. Please check username and password!"});
 });
 
 // Add a book review
