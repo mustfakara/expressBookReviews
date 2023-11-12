eae01108e4898e12ee77bcd806d06c0862253e11
mustfakara <mustikara7434@gmail.com>
2023-11-12 22:51:31 +0300

    TASK 8 : Added Book Review Method
    - Added book review which needs to be first signed in authenticated,
    checks for valid session and adds to it.
    
    - Added screenshot to folder on Postman.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,9 +4,8 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [
-  {"username": "admin", "password": "admin"},
+  {"username": "123", "password": "123"},
 ];
-
 
 // This function returns true or false in case of registering the user or in login function
 
@@ -47,10 +46,25 @@ regd_users.post("/login", (req,res) => {
   return res.status(208).json({message: "Invalid Login. Please check username and password!"});
 });
 
-// Add a book review
+// Put new reviews on books can only access users with token,
+
+// This is Task 8 *****
+
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    const reviewer = req.session.authorization['username'];
+    const newReview = req.query.review;
+    let bookReviews = books[isbn].reviews;
+
+    bookReviews[reviewer] = newReview;
+    return res.json({message: `Review has been added successfully!`, 
+                     currentUser: reviewer, 
+                     bookDetails: {"isbn": isbn, ...books[isbn]}});
+  }
+  return res.status(404).json({message: `Book with ISBN ${isbn} not found!`});
 });
 
 module.exports.authenticated = regd_users;
