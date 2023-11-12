d945fe180b4e6178d774f47a0222783381d7eef5
mustfakara <mustikara7434@gmail.com>
2023-11-12 23:07:29 +0300

    HOTFİX
    - For somehow, i have deleted the code i wrote in Task 5 commit in Task 6
    commit XD.
    
    - Added Get method of book reviews, again.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -128,9 +128,15 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+
+// This is Task 5 *****
+
+public_users.get('/review/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.json(books[isbn].reviews)
+  }
+  return res.status(404).json({message: `Book with ISBN ${isbn} not found!`});
 });
 
 module.exports.general = public_users;
